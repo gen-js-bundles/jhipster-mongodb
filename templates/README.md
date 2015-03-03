495a4e63665ff4507246cc061b69720982fe2396
ludo1026 <ludo1026@free.fr>
2015-03-03 13:53:05 +0100

    app

---
+++ templates/README.md
@@ -0,0 +1,4 @@
+README for <%=project.name.a()%>
+==========================
+
+Soon ...
---
+++ templates/{src}/PPP/config/LoggingAspectConfiguration.java
@@ -0,0 +1,33 @@
+/**
+ *  Copyright (C) 2015  <%=project.name.A()%> project org. ( http://<%=project.name.a()%>.github.io/ )
+ *
+ *  Licensed under the GNU LESSER GENERAL PUBLIC LICENSE, Version 3.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *          http://www.gnu.org/licenses/lgpl.html
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
+package <%=project.package%>.config;
+
+import <%=project.package%>.aop.logging.LoggingAspect;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
+import org.springframework.context.annotation.Profile;
+
+@Configuration
+@EnableAspectJAutoProxy
+public class LoggingAspectConfiguration {
+
+    @Bean
+    @Profile(Constants.SPRING_PROFILE_DEVELOPMENT)
+    public LoggingAspect loggingAspect() {
+        return new LoggingAspect();
+    }
+}
